2502c6cf36e0d677637709789cae16fa9e50e4ac
Hashan Liyanage <55444962+HashanLi94@users.noreply.github.com>
2020-02-22 21:48:45 +0530

    init commit

---
+++ src/app/site-header/site-header.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SiteHeaderComponent } from './site-header.component';
+
+
+
+@NgModule({
+  declarations: [SiteHeaderComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [SiteHeaderComponent]
+})
+export class SiteHeaderModule { }
